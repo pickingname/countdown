3d93f49992ca16b28ff7a0ac11eb406c039d6654
pickingname <trintrinir@icloud.com>
2025-01-25 22:03:30 +0700

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import ModeToggle from "@/components/themes/theme-switcher";
+"use client";
+
+import { Roller } from "@fecapark/number-rolling";
 
 export default function Home() {
   return (
     <main className="">
-      <ModeToggle />
+      {/* <ModeToggle /> */}
+      <div>
+        <Roller
+          value={13000}
+          suffix=""
+          suffixPosition="back"
+          align="center"
+          fontSize={48}
+          rollDuration={0.6}
+          shiftDuration={0.45}
+          staggering={false}
+          diff={false}
+          rollWay="down"
+        />
+      </div>
     </main>
   );
 }
