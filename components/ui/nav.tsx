96bf96240b0dbb31e42c5261dd5325baad732b2a
pickingname <trintrinir@icloud.com>
2025-02-14 21:09:23 +0700

    hover transition

--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -79,22 +79,22 @@ export default function Navbar() {
           href={"https://www.instagram.com/ps.onefiveseven"}
           target="_blank"
         >
-          <button className="flex items-center justify-center gap-1 h-8 px-3 text-xs font-medium text-neutral-300 bg-neutral-800 rounded-lg hover:bg-neutral-700">
+          <button className="flex items-center justify-center gap-1 h-8 px-3 text-xs font-medium text-neutral-300 bg-neutral-800 rounded-lg hover:bg-neutral-700 border-neutral-700 border transition-colors ease-in-out duration-150">
             <Instagram size={17} />
             <span className="leading-none">157 IG</span>
           </button>
         </Link>
         <Link href={"/"}>
-          <button className="flex items-center justify-center h-8 px-3 text-xs font-medium text-neutral-300 bg-neutral-800 rounded-lg hover:bg-neutral-700">
+          <button className="flex items-center justify-center h-8 px-3 text-xs font-medium text-neutral-300 bg-neutral-800 rounded-lg hover:bg-neutral-700 border-neutral-700 border transition-colors ease-in-out duration-150">
             <span className="leading-none">Countdown</span>
           </button>
         </Link>
         <Link href={"/models"}>
-          <button className="flex items-center justify-center h-8 px-3 text-xs font-medium text-neutral-300 bg-neutral-800 rounded-lg hover:bg-neutral-700">
+          <button className="flex items-center justify-center h-8 px-3 text-xs font-medium text-neutral-300 bg-neutral-800 rounded-lg hover:bg-neutral-700 border-neutral-700 border transition-colors ease-in-out duration-150">
             <span className="leading-none">3D Models</span>
           </button>
         </Link>
-        <button className="flex items-center justify-center h-8 px-3 text-xs font-medium text-neutral-900 bg-yellow-200 rounded-lg hover:bg-yellow-300">
+        <button className="flex items-center justify-center h-8 px-3 text-xs font-medium text-neutral-900 bg-yellow-200 rounded-lg hover:bg-yellow-400 transition-colors ease-in-out duration-150">
           <span className="leading-none">Board</span>
         </button>
       </div>
